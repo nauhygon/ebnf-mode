be46dd16fe7ea40dc8a7e7b44a2797896cc2a183
Tomáš Frýda <fryda.tomas@gmail.com>
2012-09-29 12:13:33 +0000

    added header

--- ebnf-mode.el
+++ ebnf-mode.el
@@ -1,3 +1,55 @@
+;;; ebnf-mode.el --- ebnf-mode is simple mode for Extended Backus-Naur Form
+;; 
+;; Filename: ebnf-mode.el
+;; Description: ebnf-mode is simple mode for Extended Backus-Naur Form.
+;; Author: Tomáš Frýda
+;; Maintainer: Tomáš Frýda
+;; Copyright (C) 2012, Tomáš Frýda, all rights reserved.
+;; Created: Sat Sep 29 12:04:06 2012 (UTC)
+;; Version: 0.0.1
+;; Last-Updated:  
+;;           By: 
+;;     Update #: 0
+;; URL: 
+;; Keywords: 
+;; Compatibility: 
+;; 
+;; Features that might be required by this library:
+;;
+;;   None
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary: 
+;; 
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change log: 
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+
 (define-derived-mode ebnf-mode
   fundamental-mode
   "ebnf-mode"
@@ -44,3 +96,6 @@
 
 (provide 'ebnf-mode)  
   
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ebnf-mode.el ends here
+
